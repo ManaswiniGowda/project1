799e1912ddcf49687af7f64083c403279800ed18
ManaswiniGowda <manaswi617@gmail.com>
2022-12-27 12:30:23 +0530

    my first commit

---
+++ strings.py
@@ -0,0 +1,20 @@
+greeting="hello"
+name="bruce"
+print(greeting+" "+name)
+print(greeting+name)
+print(greeting + name)
+print(greeting + "" + name)
+
+greeting="hello"
+name="bruce"
+print(greeting+" "+name)
+
+age=24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age="2 yrs"
+print(age)
+print(type(age))
